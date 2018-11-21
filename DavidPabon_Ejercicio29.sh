fce1445771ea53afc609c5291cd36d8f2618174e
davidalejandro04 <da.pabon10@uniandes.edu.co>
2018-11-21 06:49:22 -0500

    Script para correr todo

--- DavidPabon_Ejercicio29.sh
+++ DavidPabon_Ejercicio29.sh
@@ -1,5 +1,11 @@
 gcc -fopenmp HelloWorld.c -o HelloWorld_c
-./HelloWorld.c
+./HelloWorld_c
 
-gcc non_linear_advection.c -o -lm nla
-./nla
+gcc non_linear_advection.c -lm -o nla
+./nla 0.5
+./nla 1.0
+./nla 2.0
+./nla 4.0
+./nla 10.0
+
+python3 grafica.py
